a56c1b4060a2a230ed8cef62f2eca1d7e699fb6d
yuqi <yuqi@thunisoft.com>
2018-12-05 15:19:00 +0800

    删除敏感信息

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
-    <input class="form-control" id="inputEmail3" placeholder="请输入账号" v-model="name" />
-<input type="password" class="form-control" id="inputPassword3" placeholder="请输入密码" v-model="password" />
+    <aty-input v-model="name" />
+<aty-input v-model="password" />
 <button type="submit" class="btn btn-default" @click="login">登录</button>
-<input v-model="name" />
-<input v-model="password" />
+<aty-input v-model="name" />
+<aty-input v-model="password" />
 <button @click="submitMsg()">提交</button>
 <button @click="search()">查找信息</button>
 <div v-for="(ele, index) of userArr" :key="index">
   <span>用户姓名：{{ele.name}}</span>
   <span>用户密码：{{ele.password}}</span>
 </div>
+<aty-date-picker type="datetimerange" placeholder="选择日期和时间" style="width: 300px"></aty-date-picker>
+<!-- <div id="allmap"></div>
+		<div id="container"></div>
+		<button @click="getJsonData()">获取地区json数据</button> -->
   </div>
 </template>
 
@@ -67,10 +71,29 @@ export default {
         .catch(reject => {
           console.log(reject);
         });
+    },
+    getJsonData(){},
+    initMap(){
+      let map = new BMap.Map("allmap"); // 创建Map实例
+      map.centerAndZoom(new BMap.Point(114.502674, 38.045246), 11); // 初始化地图,设置中心点坐标和地图级别
+      //添加地图类型控件
+      map.addControl(new BMap.MapTypeControl({
+        mapTypes: [
+          BMAP_NORMAL_MAP,
+          BMAP_HYBRID_MAP
+        ]
+      }));
+      map.setCurrentCity("河北"); // 设置地图显示的城市 此项是必须设置的
+      map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
+      let cityList = new BMapLib.CityList({
+          container: 'container',
+          map: map
+      });
     }
   },
   mounted() {
     this.initData = {};
+    this.initMap();
   }
 };
 </script>
@@ -83,5 +106,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#allmap {
+  width: 100%;
+  height: 60%;
+}
+#container{
+  width: 100%;
+  height: 30%;
+}
+button{
+  height: 40px;
+  padding: 10px;
+  cursor: pointer;
+  color: white;
+  background: #3a8be9;
+  border: none;
+}
+button:hover{
+  opacity: 0.7;
 }
 </style>
